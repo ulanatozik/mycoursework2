581d88191b0d381674ed4a78389fb8c37084fa56
ulanatozik <159810554+ulanatozik@users.noreply.github.com>
2024-10-21 19:12:23 +0300

    Create Source.cpp

---
+++ Source.cpp
@@ -0,0 +1,15 @@
+//
+//  Source.cpp
+//  coursework yanechka
+//
+//  Created by Ульяна Тозик on 12.04.24.
+//
+
+#include "Header.h"
+
+int main() {
+    Menu menuObj;
+    setlocale(0, "rus");
+    menuObj.start_menu();
+}
+
